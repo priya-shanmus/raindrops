29b8cf414e5a53854abdfd7d83b25eab312a469e
priya-shanmus <70858619+priya-shanmus@users.noreply.github.com>
2020-11-29 16:42:24 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,19 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("Walking Frame/walking_1.png","Walking Frame/walking_2.png","Walking Frame/walking_3.png",
+        "Walking Frame/walking_4.png","Walking Frame/walking_5.png","Walking Frame/walking_6.png","Walking Frame/walking_7.png",
+        "Walking Frame/walking_8.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);
+    }
+}
